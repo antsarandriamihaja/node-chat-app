636cb61b8023a461d729deaaf64d7794bd9ea30a
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-04-13 13:59:30 -0400

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,10 @@ socket.on('disconnect', function () {
 //listening to events from server
 socket.on('newMessage', (message)=>{
     console.log('newMessage', message);
+    //create an element with jQuery
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 })
 socket.emit('createMessage', {
     from: 'Antsa',
@@ -25,4 +29,11 @@ socket.emit('createMessage', {
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function(){
+
+    })
 })
